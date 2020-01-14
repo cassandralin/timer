0545d86791d3e7534fb3a8c3a5781feacc7e038b
cassandralin <cassandralin92@gmail.com>
2020-01-14 21:04:16 +0000

    worked with Shivam to adjust code for edge cases

--- timer1.js
+++ timer1.js
@@ -1,12 +1,14 @@
 const input = process.argv.slice(2)
 
 const simpleTimer = (input) => {
-  input.forEach(n => {
+  input 
+  .map(n => parseInt(n))
+  .filter(n => Number.isInteger(n) && n >= 0)
+  .forEach(n => {
     setTimeout(() => process.stdout.write('\x07'), n);
   });
 };
 
 simpleTimer(input);
-
 
 
